ef8999e69ef8ed9785b993b90ef77ee4c092f1bf
Jack McNicol <jackmcpickle@gmail.com>
2016-07-05 22:40:19 +0930

    update sass-true

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
-var path = require('path');
-var sassTrue = require('sass-true');
+const path     = require('path');
+const sassTrue = require('sass-true');
+const sassFile = path.join(__dirname, './all.scss');
 
-var sassFile = path.join(__dirname, './all.scss');
 sassTrue.runSass({file: sassFile}, describe, it);
