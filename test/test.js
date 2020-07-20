d1f7deb68d9d6c664353ac8bc7953a0e54923a3e
Jack McNicol <jack.mcnicol@aligent.com.au>
2020-07-20 09:50:19 +0930

    update testing to latest versions

--- test/test.js
+++ test/test.js
@@ -2,4 +2,4 @@ const path     = require('path');
 const sassTrue = require('sass-true');
 const sassFile = path.join(__dirname, './all.scss');
 
-sassTrue.runSass({file: sassFile}, describe, it);
+sassTrue.runSass({file: sassFile}, { describe, it });
